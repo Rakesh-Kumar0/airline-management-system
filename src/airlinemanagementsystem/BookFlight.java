82b50055841775b95f0364ac1ab97e2b967ee02c
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-04-29 23:40:39 +0530

    adding new file

--- src/airlinemanagementsystem/BookFlight.java
+++ src/airlinemanagementsystem/BookFlight.java
@@ -5,3 +5,22 @@ import java.awt.event.*;
 import java.sql.*;
 import com.toedter.calendar.JDateChooser;
 import java.util.*;
+
+
+public class BookFlight extends JFrame implements ActionListener{
+    
+    JTextField tfaadhar;
+    JLabel tfname, tfnationality, tfaddress, labelgender, labelfname, labelfcode;
+    JButton bookflight, fetchButton, flight;
+    Choice source, destination;
+    JDateChooser dcdate;
+    
+    public BookFlight() {
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Book Flight");
+        heading.setBounds(420, 20, 500, 35);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        heading.setForeground(Color.BLUE);
+        add(heading);
