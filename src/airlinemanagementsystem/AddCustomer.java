7d960727482cf0db62444b59e42ced9081aa06da
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-04-29 23:33:24 +0530

    adding new file

--- src/airlinemanagementsystem/AddCustomer.java
+++ src/airlinemanagementsystem/AddCustomer.java
@@ -18,3 +18,21 @@ public class AddCustomer extends JFrame implements ActionListener{
         heading.setFont(new Font("Tahoma", Font.PLAIN, 32));
         heading.setForeground(Color.BLUE);
         add(heading);
+
+        JLabel lblname = new JLabel("Name");
+        lblname.setBounds(60, 80, 150, 25);
+        lblname.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblname);
+        
+        tfname = new JTextField();
+        tfname.setBounds(220, 80, 150, 25);
+        add(tfname);
+        
+        JLabel lblnationality = new JLabel("Nationality");
+        lblnationality.setBounds(60, 130, 150, 25);
+        lblnationality.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblnationality);
+        
+        tfnationality = new JTextField();
+        tfnationality.setBounds(220, 130, 150, 25);
+        add(tfnationality);
