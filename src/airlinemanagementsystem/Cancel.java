316b7b9e11025c0c50c0c4f27035cb51dab80673
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-04-29 23:50:06 +0530

    adding new file

---
+++ src/airlinemanagementsystem/Cancel.java
@@ -0,0 +1,24 @@
+package airlinemanagementsystem;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
+
+public class Cancel extends JFrame implements ActionListener{
+    
+    JTextField tfpnr;
+    JLabel tfname, cancellationno, lblfcode, lbldateoftravel;
+    JButton fetchButton, flight;
+    
+    public Cancel() {
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        Random random = new Random();
+        
+        JLabel heading = new JLabel("CANCELLATION");
+        heading.setBounds(180, 20, 250, 35);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        add(heading);
--- src/airlinemanagementsystem/Cancle.java
+++
@@ -1,7 +0,0 @@
-package airlinemanagementsystem;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.sql.*;
-import java.util.*;
