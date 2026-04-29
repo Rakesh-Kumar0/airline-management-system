c6946a069e8cdc1df521320f7085378d3d07a053
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-04-29 23:50:36 +0530

    adding new file

--- src/airlinemanagementsystem/Cancel.java
+++ src/airlinemanagementsystem/Cancel.java
@@ -22,3 +22,19 @@ public class Cancel extends JFrame implements ActionListener{
         heading.setBounds(180, 20, 250, 35);
         heading.setFont(new Font("Tahoma", Font.PLAIN, 32));
         add(heading);
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("airlinemanagementsystem/icons/cancel.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(470, 120, 250, 250);
+        add(image);
+        
+        JLabel lblaadhar = new JLabel("PNR Number");
+        lblaadhar.setBounds(60, 80, 150, 25);
+        lblaadhar.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lblaadhar);
+        
+        tfpnr = new JTextField();
+        tfpnr.setBounds(220, 80, 150, 25);
+        add(tfpnr);
