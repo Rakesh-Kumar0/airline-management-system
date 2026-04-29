aefe67ad26a667ee0fd83ff6c6fd9899b9f50c29
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-04-29 23:32:56 +0530

    adding new file

--- src/airlinemanagementsystem/AddCustomer.java
+++ src/airlinemanagementsystem/AddCustomer.java
@@ -3,3 +3,18 @@ package airlinemanagementsystem;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
+public class AddCustomer extends JFrame implements ActionListener{
+    
+    JTextField tfname, tfphone, tfaadhar, tfnationality, tfaddress;
+    JRadioButton rbmale, rbfemale;
+    
+    public AddCustomer() {
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("ADD CUSTOMER DETAILS");
+        heading.setBounds(220, 20, 500, 35);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        heading.setForeground(Color.BLUE);
+        add(heading);
