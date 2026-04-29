31b38adab38d37e7beb35f436f7adc6c06508cf4
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-04-29 23:54:10 +0530

    adding new file

--- src/airlinemanagementsystem/BoardingPass.java
+++ src/airlinemanagementsystem/BoardingPass.java
@@ -5,3 +5,24 @@ import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
 import java.util.*;
+
+public class BoardingPass extends JFrame implements ActionListener{
+    
+    JTextField tfpnr;
+    JLabel tfname, tfnationality, lblsrc, lbldest, labelfname, labelfcode, labeldate;
+    JButton fetchButton;
+    
+    public BoardingPass() {
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("AIR INDIA");
+        heading.setBounds(380, 10, 450, 35);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 32));
+        add(heading);
+        
+        JLabel subheading = new JLabel("Boarding Pass");
+        subheading.setBounds(360, 50, 300, 30);
+        subheading.setFont(new Font("Tahoma", Font.PLAIN, 24));
+        subheading.setForeground(Color.BLUE);
+        add(subheading);
