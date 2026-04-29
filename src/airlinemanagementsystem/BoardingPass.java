d2c9ea02ce42938706da7f70b39d4c9e23b4ce9a
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-04-29 23:55:59 +0530

    adding new file

--- src/airlinemanagementsystem/BoardingPass.java
+++ src/airlinemanagementsystem/BoardingPass.java
@@ -96,3 +96,24 @@ public class BoardingPass extends JFrame implements ActionListener{
         labelfcode = new JLabel();
         labelfcode.setBounds(540, 260, 150, 25);
         add(labelfcode);
+
+         JLabel lbldate = new JLabel("Date");
+        lbldate.setBounds(60, 300, 150, 25);
+        lbldate.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        add(lbldate);
+        
+        labeldate = new JLabel();
+        labeldate.setBounds(220, 300, 150, 25);
+        add(labeldate);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("airlinemanagementsystem/icons/airindia.png"));
+        Image i2 = i1.getImage().getScaledInstance(300, 230, Image.SCALE_DEFAULT);
+        ImageIcon image = new ImageIcon(i2);
+        JLabel lblimage = new JLabel(image);
+        lblimage.setBounds(600, 0, 300, 300);
+        add(lblimage);
+        
+        setSize(1000, 450);
+        setLocation(300, 150);
+        setVisible(true);
+    }
