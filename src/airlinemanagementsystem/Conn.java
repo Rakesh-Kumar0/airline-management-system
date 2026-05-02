e9121998f38ddd9dfb690f23ac34a207c485334b
Rakesh-Kumar0 <rakeshk49312@gmail.com>
2026-05-02 22:59:44 +0530

    modified

--- src/airlinemanagementsystem/Conn.java
+++ src/airlinemanagementsystem/Conn.java
@@ -8,6 +8,7 @@ public class Conn {
     public Conn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
+            // Establish a connection to the database
             c = DriverManager.getConnection("jdbc:mysql://localhost:3306/airlinemanagementsystem", "root", "8687rakesh@");// use your own database credentials
             s = c.createStatement();
         } catch (Exception e) {
